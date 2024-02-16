4bff0e9e533ca41b1e098514788138ec31574b4e
hector Rivera <hrolriverav@gmail.com>
2024-02-16 14:07:18 -0600

    start

--- examen5P2_HectorRivera/src/examen5p2_hectorrivera/examen5P2_HectorRivera.java
+++ examen5P2_HectorRivera/src/examen5p2_hectorrivera/examen5P2_HectorRivera.java
@@ -2,17 +2,23 @@
  */
 package examen5p2_hectorrivera;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author hriverav
  */
-public class examen5P2_HectorRivera extends javax.swing.JFrame {
-
+public class Examen5P2_HectorRivera extends javax.swing.JFrame {
+    
     /**
      * Creates new form examen5P2_HectorRivera
      */
-    public examen5P2_HectorRivera() {
+    public Examen5P2_HectorRivera() {
         initComponents();
+        usuarios.add(new empleados());
+        
+        usuarios.add(new Civilies("jaf","algo","121xc","male","Francisco Morazan","",new Date(2004, 3, 12)));
     }
 
     /**
@@ -24,17 +30,30 @@ public class examen5P2_HectorRivera extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -57,24 +76,28 @@ public class examen5P2_HectorRivera extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(examen5P2_HectorRivera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Examen5P2_HectorRivera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(examen5P2_HectorRivera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Examen5P2_HectorRivera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(examen5P2_HectorRivera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Examen5P2_HectorRivera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(examen5P2_HectorRivera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Examen5P2_HectorRivera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new examen5P2_HectorRivera().setVisible(true);
+                new Examen5P2_HectorRivera().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
+    ArrayList<Usuario> usuarios = new ArrayList<>();
+    
 }
---
+++ examen5P2_HectorRivera/src/examen5p2_hectorrivera/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen5p2_hectorrivera;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author hriverav
+ */
+public class main {
+    public static void main(String[] args){
+        Examen5P2_HectorRivera Frame = new Examen5P2_HectorRivera();
+        Frame.setVisible(true);
+        Frame.pack();   
+    }
+}
